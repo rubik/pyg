e5faf3d83a5dfba0e8e295e118f7904bccf5f70c
Michele Lacchia <michelelacchia@gmail.com>
2011-02-28 17:26:05 +0100

    Added a Version class to manage and compare different versions

--- pyg/req.py
+++ pyg/req.py
@@ -1,4 +1,67 @@
 import operator
+import itertools
+
+
+class Version(object):
+    def __init__(self, v):
+        self.v = v
+        self.parts = v.split('.')
+
+    def __repr__(self):
+        return 'Version({0})'.format(self.v)
+
+    def __eq__(self, other):
+        if len(self.parts) != len(other.parts):
+            return False
+        return all(p == p2 for p, p2 in itertools.izip(self.parts, other.parts))
+
+    def __ge__(self, other):
+        for p, p2 in itertools.izip_longest(self.parts, other.parts):
+            if p is None and p2 is not None:
+                return False
+            elif p2 is None:
+                return True
+            elif p >= p2:
+                return True
+            elif p < p2:
+                return False
+        return False
+
+    def __gt__(self, other):
+        for p, p2 in itertools.izip_longest(self.parts, other.parts):
+            if p is None and p2 is not None:
+                return False
+            elif p2 is None:
+                return True
+            elif p > p2:
+                return True
+            elif p < p2:
+                return False
+        return False
+
+    def __le__(self, other):
+        for p, p2 in itertools.izip_longest(self.parts, other.parts):
+            if p is None and p2 is not None:
+                return True
+            elif p2 is None:
+                return False
+            elif p <= p2:
+                return True
+            elif p > p2:
+                return False
+        return False
+
+    def __lt__(self, other):
+        for p, p2 in itertools.izip_longest(self.parts, other.parts):
+            if p is None and p2 is not None:
+                return True
+            elif p2 is None:
+                return False
+            elif p < p2:
+                return True
+            elif p > p2:
+                return False
+        return False
 
 
 class Requirement(object):
@@ -18,10 +81,10 @@ class Requirement(object):
         for c in ('==', '>=', '>', '<=', '<'):
             if c in self.req:
                 self.name, self.op, self.version = map(str.strip, self.req.partition(c))
-                self.version = float(self.version)
+                self.version = Version(self.version)
                 break
         else:
-            raise ValueError('The request does not make any sense') ## FIXME: We have to search the newest
+            self.version = None
 
     @ staticmethod
     def find_version(s):
@@ -31,7 +94,7 @@ class Requirement(object):
                 v.append(c)
             else:
                 break
-        return float(''.join(v).strip('.')) ## FIXME do we really need .strip() ?
+        return Version(''.join(v).strip('.')) ## FIXME do we really need .strip() ?
         
 
     def best_match(self, reqs):
@@ -39,10 +102,11 @@ class Requirement(object):
         for r in reqs:
             parts = r.split('-')
             version = Requirement.find_version('-'.join(parts[1:]))
-            if self.OPMAP[self.op](version, self.version):
+            if self.version is None or self.OPMAP[self.op](version, self.version):
                 matched[version] = r
         if len(matched) == 0:
             return None
         elif len(matched) == 1:
             return matched[matched.keys()[0]]
-        return matched[sorted(matched.keys(), reverse=True)[0]] ## The highest version possible
+        ## The highest version possible
+        return matched[max(matched)] ## OR matched[sorted(matched.keys(), reverse=True)[0]]?
