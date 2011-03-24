456f13f3cf2163db61531b5cd340fdabecd0be12
Michele Lacchia <michelelacchia@gmail.com>
2011-03-24 10:20:31 +0100

    Fixed for readthedocs.org (3)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,13 @@ import pyg
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinxtogithub']
+extensions = ['sphinx.ext.autodoc']
+try:
+    import sphinxtogithub
+except ImportError:
+    pass
+else:
+    extensions.append('sphinxtogithub')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
