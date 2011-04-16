848aed9840d8aa07a523e1460a2a70323b70a90d
Michele Lacchia <michelelacchia@gmail.com>
2011-04-16 09:24:27 +0200

    Added other VCSs

--- pyg/vcs/vcs.py
+++ pyg/vcs/vcs.py
@@ -1,4 +1,7 @@
-from pyg.vcs import VCS
+from pyg.vcs.base import VCS
+
+
+__all__ = ['Git', 'Hg', 'Bzr', 'Svn']
 
 
 class Git(VCS):
@@ -11,3 +14,39 @@ class Git(VCS):
             url = url[4:]
         self.url, self.package_name = self.parse_url(url)
         super(Git, self).__init__(dest or self.package_name)
+
+
+class Hg(VCS):
+
+    CMD = 'hg'
+    METHOD = 'clone'
+
+    def __init__(self, url, dest=None):
+        if url.startswith('hg+'):
+            url = url[3:]
+        self.url, self.package_name = self.parse_url(url)
+        super(Hg, self).__init__(dest or self.package_name)
+
+
+class Bzr(VCS):
+
+    CMD = 'bzr'
+    METHOD = 'branch'
+
+    def __init__(self, url, dest=None):
+        if url.startswith('bzr+'):
+            url = url[4:]
+        self.url, self.package_name = self.parse_url(url)
+        super(Bzr, self).__init__(dest or self.package_name)
+
+
+class Svn(VCS):
+
+    CMD = 'svn'
+    METHOD = 'checkout'
+
+    def __init__(self, url, dest=None):
+        if url.startswith('svn+'):
+            url = url[4:]
+        self.url, self.package_name = self.parse_url(url)
+        super(Svn, self).__init__(dest or self.package_name)
