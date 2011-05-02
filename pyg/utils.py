ba13dec0a989afbf8a57377e1fb61189982b1eb5
Michele Lacchia <michelelacchia@gmail.com>
2011-05-02 17:26:11 +0200

    Added the subprocess error for Py2.6

--- pyg/utils.py
+++ pyg/utils.py
@@ -21,6 +21,19 @@ PYTHON_VERSION = '.'.join(map(str, sys.version_info[:2]))
 try:
     check_output = subprocess.check_output
 except AttributeError:
+    class CalledProcessError(Exception):
+        """This exception is raised when a process run by check_call() or
+        check_output() returns a non-zero exit status.
+        The exit status will be stored in the returncode attribute;
+        check_output() will also store the output in the output attribute.
+        """
+        def __init__(self, returncode, cmd, output=None):
+            self.returncode = returncode
+            self.cmd = cmd
+            self.output = output
+        def __str__(self):
+            return "Command '%s' returned non-zero exit status %d" % (self.cmd, self.returncode)
+
     def check_output(*popenargs, **kwargs):
         '''Copied from Python2.7 subprocess.py'''
         if 'stdout' in kwargs:
@@ -32,7 +45,7 @@ except AttributeError:
             cmd = kwargs.get("args")
             if cmd is None:
                 cmd = popenargs[0]
-            raise subprocess.CalledProcessError(retcode, cmd, output=output)
+            raise CalledProcessError(retcode, cmd, output=output)
         return output
 
 def is_installed(req):
