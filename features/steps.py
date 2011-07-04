17a95d71aad9593b13fce97268cef7bd2845fd77
fab <fdev31@gmail.com>
2011-07-04 16:32:46 +0200

    test steps: Fix virtualenv usage

--- features/steps.py
+++ features/steps.py
@@ -1,13 +1,41 @@
+import os
 from lettuce import *
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
+
+VENV_DIR="/tmp/test_envs"
+
+@before.all
+def init_env(*a):
+    """ Ensure VENV_DIR folder is empty """
+    import shutil
+    try:
+        shutil.rmtree(VENV_DIR)
+    except OSError:
+        pass
+    os.mkdir(VENV_DIR)
 
 @step('I use "(.*)" environment')
 def given_i_use_venv(step, env_name):
     world.env_name = env_name
+    world.env_path = os.path.join(VENV_DIR, env_name)
+
+    # Create Venv if it can't be found
+    if not os.path.exists(world.env_path):
+        call(['virtualenv', '--no-site-packages', world.env_path])
+
+    # Install pyg if not found
+    if not os.path.exists(os.path.join(world.env_path, 'bin', 'pyg')):
+
+        pyg_dir = os.path.dirname(os.path.dirname(__file__))
+        call([ os.path.join(world.env_path, 'bin', 'python'),
+            os.path.join(pyg_dir, 'setup.py'),
+            'install'
+            ])
 
 @step('I execute +(.*)')
 def i_execute(step, cmd):
-    world.proc = Popen(cmd.split(), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    world.proc = Popen(os.path.join(world.env_path, 'bin', cmd),
+     stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
 @step('the return code is (\d+)')
 def the_return_code_is(step, code):
