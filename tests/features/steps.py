91d081610557ad54465a5000dfff57eb77348c8a
fab <fdev31@gmail.com>
2011-07-13 22:14:05 +0200

    tests: Do not use proxy by default (not mature yet)

--- tests/features/steps.py
+++ tests/features/steps.py
@@ -8,7 +8,7 @@ import itertools
 from glob import glob
 from subprocess import Popen, PIPE, call
 
-USE_PROXY = True
+USE_PROXY = False # TODO: pypi_cache_server is not stable enough
 
 VENV_DIR = os.getenv('KEEPENV', None) or tempfile.mkdtemp(prefix='pyg_env_')
 
